42711722acee6bbac46e780c990149b35ae0d26f
vsyroc <vsyroc@gmail.com>
2026-03-29 17:25:29 +0500

    initial commit

---
+++ app/__main__.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(title="Carinpainting")
+
+
+@app.get("/")
+async def index():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
